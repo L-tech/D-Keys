c00a75268af2c6765b047e24de44fa6ee27b6cc1
L-tech <legendabrahamonoja@gmail.com>
2022-01-17 23:33:09 +0100

    update: frontend update

--- pages/index.js
+++ pages/index.js
@@ -1,143 +1,136 @@
-import { ethers } from "ethers";
-import Router from "next/router";
 import { useState, useEffect } from "react";
 import PrimaryButton from "../components/primary-button";
+import Keyboard from "../components/keyboard";
 import abi from "../utils/Keyboards.json"
+import { ethers } from "ethers";
 
-export default function Create() {
 
-    const [ethereum, setEthereum] = useState(undefined);
-    const [connectedAccount, setConnectedAccount] = useState(undefined);
+export default function Home() {
+  const [ethereum, setEthereum] = useState(undefined);
+  const [connectedAccount, setConnectedAccount] = useState(undefined);
+  const [keyboards, setKeyboards] = useState([])
+  const [newKeyboard, setNewKeyboard] = useState("")
+  const [keyboardsLoading, setKeyboardsLoading] = useState(false);
+  const contractAddress = '0x6d66A4d88B59A804Ef5176740ffF7759E606c6bE';
+  const contractABI = abi.abi;
 
-    const [keyboardKind, setKeyboardKind] = useState(0)
-    const [isPBT, setIsPBT] = useState(false)
-    const [filter, setFilter] = useState('')
-
-    const contractAddress = '0x9C71bD759df479276F6eB215471aFF64cbf910A3';
-    const contractABI = abi.abi;
-
-    const handleAccounts = (accounts) => {
-      if (accounts.length > 0) {
-        const account = accounts[0];
-        console.log('We have an authorized account: ', account);
-        setConnectedAccount(account);
-      } else {
-        console.log("No authorized accounts yet")
-      }
-    };
-
-    const getConnectedAccount = async () => {
-      if (window.ethereum) {
-        setEthereum(window.ethereum);
-      }
-
-      if (ethereum) {
-        const accounts = await ethereum.request({ method: 'eth_accounts' });
-        handleAccounts(accounts);
-      }
-    };
-    useEffect(() => getConnectedAccount(), []);
-
-    const connectAccount = async () => {
-      if (!ethereum) {
-        alert('MetaMask is required to connect an account');
-        return;
-      }
-
-      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-      handleAccounts(accounts);
-    };
-
-    const submitCreate = async (e) => {
-      e.preventDefault();
-
-      if (!ethereum) {
-        console.error('Ethereum object is required to create a keyboard');
-        return;
-      }
-
-      const provider = new ethers.providers.Web3Provider(ethereum);
-      const signer = provider.getSigner();
-      const keyboardsContract = new ethers.Contract(contractAddress, contractABI, signer);
-
-      const createTxn = await keyboardsContract.create(keyboardKind, isPBT, filter)
-      console.log('Create transaction started...', createTxn.hash)
-
-      await createTxn.wait();
-      console.log('Created keyboard!', createTxn.hash);
-
-      Router.push('/');
+  const handleAccounts = (accounts) => {
+    if (accounts.length > 0) {
+      const account = accounts[0];
+      console.log('We have an authorized account: ', account);
+      setConnectedAccount(account);
+    } else {
+      console.log("No authorized accounts yet")
     }
+  };
+  
+  const getConnectedAccount = async () => {
+    if (window.ethereum) {
+      setEthereum(window.ethereum);
+    }
+  
+    if (ethereum) {
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+      handleAccounts(accounts);
+    }
+  };
+  useEffect(() => getConnectedAccount(), []);
+  
+  const connectAccount = async () => {
+    if (!ethereum) {
+      alert('MetaMask is required to connect an account');
+      return;
+    }
+  
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    handleAccounts(accounts);
+  };
+  const getKeyboards = async () => {
+    if (ethereum && connectedAccount) {
+      setKeyboardsLoading(true);
+      try {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const keyboardsContract = new ethers.Contract(contractAddress, contractABI, signer);
+  
+        const keyboards = await keyboardsContract.getKeyboards();
+        console.log('Retrieved keyboards...', keyboards)
+        
+        setKeyboards(keyboards)
+      } finally {
+        setKeyboardsLoading(false);
+      }
+    }
+  }
+  useEffect(() => getKeyboards(), [connectedAccount])
+  const submitCreate = async (e) => {
+    e.preventDefault();
 
     if (!ethereum) {
-      return <p>Please install MetaMask to connect to this site</p>
+      console.error('Ethereum object is required to create a keyboard');
+      return;
     }
 
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const keyboardsContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+    const createTxn = await keyboardsContract.create(newKeyboard);
+    console.log('Create transaction started...', createTxn.hash);
+
+    await createTxn.wait();
+    console.log('Created keyboard!', createTxn.hash);
+
+    await getKeyboards();
+  }
+
+  if (!ethereum) {
+    return <p>Please install MetaMask to connect to this site</p>
+  }
+
+  if (!connectedAccount) {
+    return <PrimaryButton onClick={connectAccount}>Connect MetaMask Wallet</PrimaryButton>
+  }
+
+  if (!ethereum) {
+    return <p>Please install MetaMask to connect to this site</p>
+  }
+  
     if (!connectedAccount) {
       return <PrimaryButton onClick={connectAccount}>Connect MetaMask Wallet</PrimaryButton>
     }
-
+  
+    if (keyboards.length > 0) {
+      return (
+        <div className="flex flex-col gap-4">
+          <PrimaryButton type="link" href="/create">Create a Keyboard!</PrimaryButton>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-2 p-2">
+            {keyboards.map(
+              ([kind, isPBT, filter], i) => (
+                <Keyboard key={i} kind={kind} isPBT={isPBT} filter={filter} />
+              )
+            )}
+          </div>
+        </div>
+      )
+    }
+    if (keyboardsLoading) {
+      return (
+        <div className="flex flex-col gap-4">
+          <PrimaryButton type="link" href="/create">Create a Keyboard!</PrimaryButton>
+          <p>Loading Amazing Keyboards...</p>
+        </div>
+      )
+    }
+    
+  
+    // No keyboards yet
     return (
-      <div className="flex flex-col gap-y-8">
-        <form className="mt-8 flex flex-col gap-y-6">
-          <div>
-            <label htmlFor="keyboard-type" className="block text-sm font-medium text-gray-700">
-              Keyboard Type
-            </label>
-            <select
-              id="keyboard-type"
-              name="keyboard-type"
-              className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-              value={keyboardKind}
-              onChange={(e) => { setKeyboardKind(e.target.value) }}
-            >
-              <option value="0">60%</option>
-              <option value="1">75%</option>
-              <option value="2">80%</option>
-              <option value="3">ISO-105</option>
-            </select>
-          </div>
-
-          <div>
-            <label htmlFor="keycap-type" className="block text-sm font-medium text-gray-700">
-              Keycap Type
-            </label>
-            <select
-              id="keycap-type"
-              name="keycap-type"
-              className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-              value={isPBT ? "pbt" : "abs"}
-              onChange={(e) => { setIsPBT(e.target.value === "pbt") }}
-            >
-              <option value="abs">ABS</option>
-              <option value="pbt">PBT</option>
-            </select>
-          </div>
-
-          <div>
-            <label htmlFor="filter" className="block text-sm font-medium text-gray-700">
-              Filter
-            </label>
-            <select
-              id="filter"
-              name="filter"
-              className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-              onChange={(e) => { setFilter(e.target.value) }}
-              value={filter}
-            >
-              <option value="">None</option>
-              <option value="sepia">Sepia</option>
-              <option value="grayscale">Grayscale</option>
-              <option value="invert">Invert</option>
-              <option value="hue-rotate-90">Hue Rotate (90°)</option>
-              <option value="hue-rotate-180">Hue Rotate (180°)</option>
-            </select>
-          </div>
-
-          <PrimaryButton type="submit" onClick={submitCreate}>
-            Create Keyboard!
-          </PrimaryButton>
-        </form>
+      <div className="flex flex-col gap-4">
+        <PrimaryButton type="link" href="/create">Create a Keyboard!</PrimaryButton>
+        <p>No keyboards yet!</p>
       </div>
     )
+  
 }
+
